b7a53442bf37a88e90f13b95ec17a386d51f1298
pascalopitz <contact@pascalopitz.com>
2011-08-27 23:55:11 +0100

    removed pre-saved html fields added markdown filter

---
+++ filters.py
@@ -0,0 +1,9 @@
+import markdown as md
+
+from google.appengine.ext import webapp
+
+register = webapp.template.create_template_register()
+
+@register.filter
+def markdown(value):
+    return md.markdown(str(value))
