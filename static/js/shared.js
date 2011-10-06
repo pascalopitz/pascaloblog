04fb8ae66004ad0a0786d850b15e7eec3812d867
Pascal Opitz <contact@pascalopitz.com>
2011-10-06 12:11:19 +0100

    viewport scroll

--- static/js/shared.js
+++ static/js/shared.js
@@ -1,18 +1,33 @@
 $(function() {
-    $("select, input:checkbox, input:radio, input:file, input:text, textarea, button, a.button").uniform();
-
-    window.setTimeout(function() {
-        $('.message').fadeOut();
-    }, 3000);
-
-    $('body').delegate('.show-more', 'click', function(e) {
+    function initForms() {
+        $("select, input:checkbox, input:radio, input:file, input:text, textarea, button, a.button").filter(':not(.uniform)').addClass('uniform').uniform();
+    }
+    
+    function loadMore(e) {
         var link = this;
         
         $.get($(link).attr('href'), function(data) {
             $(link).before(data);
+            initForms();
             $(link).remove();
         });
         
         return false;
+    }
+
+    window.setTimeout(function() {
+        $('.message').fadeOut();
+    }, 3000);
+    
+    initForms();
+
+    $('body').delegate('.show-more', 'click', function(e) {
+        loadMore.call(this, e);
+    });
+    
+    $(document).bind('scroll', function(e) {
+        if($('.show-more:in-viewport').length) {
+            loadMore.call($('.show-more:in-viewport').get(0), e);
+        }
     });
 });
